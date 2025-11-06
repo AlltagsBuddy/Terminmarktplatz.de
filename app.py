d4b737a04903527013a7c5645db80cae71061d2a
AlltagsBuddy <info@alltags-buddy.de>
2025-11-06 13:59:11 +0100

    Neu

--- app.py
+++ app.py
@@ -1131,7 +1131,7 @@ def _haversine_km(lat1, lon1, lat2, lon2):
     dlat = radians(lat2 - lat1)
     dlon = radians(lon2 - lon1)
     a = sin(dlat/2)**2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon/2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    c = 2 * atan2(sqrt(a), sqrt(a))
     return R * c
 
 @app.get("/public/slots")
