b83aba8c5b437a07dd2d1c2f9d82297cabbe8e9c
AlltagsBuddy <info@alltags-buddy.de>
2025-12-21 13:33:10 +0100

    Backup

--- app.py
+++ app.py
@@ -4096,3 +4096,4 @@ def public_cancel():
 # --------------------------------------------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    
